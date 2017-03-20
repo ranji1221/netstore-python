bf24f9c1bffcce1a027a298cc64f1e84aa370761
jiran1221 <jiran1221@163.com>
2017-03-20 17:12:30 +0800

    ʹÓÃעÊͱàд¹æ¸ñµÄÊ׸ö¹¦ÄܲâÊԣ¬¶øÇÒʹÓÃÁËunittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,28 @@
 from selenium import webdriver
+import unittest
 
-#browser = webdriver.Firefox()
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+		self.browser.implicitly_wait(3)
+	def setDown(self):
+		self.browser.quit()
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#伊迪斯听说有一个很酷的在线待办事项应用
+		#她去看了这个应用的首页
+		self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+		#她注意到网页的标题和头部都包含了"To-Do"这个词
+		self.assertIn('To-Do',self.browser.title)
+		self.fail('Finish the test!')
+		
+		self.assertIn('Django',self.browser.title)
+		#应用邀请她输入一个待办事项
+
+		#她在一个文本框
+
+		#她很满意，去睡觉了
+		
+if __name__ == '__main__':
+	unittest.main()
+

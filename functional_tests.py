bec9e14b4981cbaaa7021394076c5d7f852a10c1
jiran1221 <jiran1221@163.com>
2017-03-22 12:04:22 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,7 +18,6 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do',self.browser.title)
 		headerText = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',headerText)
-		self.fail('Finish the test!')
 		
 		#应用邀请她输入一个待办事项
 		inputBox = self.browser.find_element_by_id('id_new_item')
@@ -38,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			'New to-do item did not appear in table'
 		)
 		
 		#页面中又显示了一个文本框，可以输入其他的待办事项

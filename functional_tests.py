965b8c86c9f5f6f700ab9f01dcda64e613959aca
jiran1221 <jiran1221@163.com>
2017-03-24 13:58:37 +0800

    modify functional_test.py file for less code by define function

--- functional_tests.py
+++ functional_tests.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Chrome()       #需要下载chrome浏览器的驱动，并在环境变量path中指定
 		#self.browser = webdriver.Firefox()		#需要下载firefox浏览器的驱动，并在环境变量path中指定
 		self.browser.implicitly_wait(3)      #如果用火狐浏览器似乎这个代码有个bug
-	def setDown(self):
+	def tearDown(self):
 		self.browser.quit()
+	
+	def check_for_row_in_list_table(self,row_text):
+		table =  self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#伊迪斯听说有一个很酷的在线待办事项应用
 		#她去看了这个应用的首页
@@ -32,6 +39,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 		#她按回车键后，页面更新了
 		#待办事项表格中显示了"1: Buy peacock feathers"
+		time.sleep(1)
 		inputBox.send_keys(Keys.ENTER)
 		
 		#import time
@@ -53,14 +61,20 @@ class NewVisitorTest(unittest.TestCase):
 		#伊迪斯做事儿很有条理
 		inputBox = self.browser.find_element_by_id('id_new_item')
 		inputBox.send_keys('Use peacock feathers to make a fly')
+		time.sleep(1)
 		inputBox.send_keys(Keys.ENTER)
 		
-		import time
-		time.sleep(10)
+		
+		time.sleep(3)
+		''' 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		'''
+		#重构代码，写成函数更加的好调用
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.fail('Finish the test!')
 		
 		#页面再次更新，她的清单中显示了这两个待办事项
